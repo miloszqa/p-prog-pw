d4d99c3af45729e4f8a24ec1e59104f99a0f4bcc
Mil Ban <kutasaki1@wp.pl>
2024-09-07 14:44:40 +0200

    Added steps for calendar picker with verification of the date

--- tests/formTests.spec.ts
+++ tests/formTests.spec.ts
@@ -1,5 +1,6 @@
-import { expect, test, Browser } from "@playwright/test"
+import { expect, test } from "@playwright/test"
 import { LandingPage } from "../page-object-model/landingPage"
+import { getDateMinusFourYears } from "../utils/getDateMinusFourYears"
 
 test.beforeEach(async({ page }) => {
     await page.goto('/')
@@ -12,7 +13,7 @@ test('verify mandatory fields', async({ page} ) => {
     const errorMessages = ['Pole Imię jest wymagane', 'Pole Nazwisko jest wymagane', 'Pole E-mail jest wymagane', 'Pole password jest wymagane', 'Pole Powtórz hasło jest wymagane', 'Pole Data urodzenia jest wymagane']
 
     for(let placeholder of placeHolderValues) {
-        await landingPage.validateMandatoryFields(placeholder);
+        await landingPage.validateMandatoryFields(placeholder)
         for(let error of errorMessages) {
             expect(landingPage.formContainer.getByText(error))
         }
@@ -24,12 +25,19 @@ test.only('create user with valid form data', async( {page} ) => {
     const landingPage = new LandingPage(page)
 
     const placeHolderValues: string[] = ['Imię', 'Nazwisko', 'Twój adres e-mail', 'Hasło','Powtórz hasło']
-    let data: string[] = ['Testname', 'Testsurname', 'valid@email.com', 'ValidPassword1234!', 'ValidPassword1234!']
+    const data: string[] = ['Testname', 'Testsurname', 'valid@email.com', 'ValidPassword1234!', 'ValidPassword1234!']
 
     for (let i = 0; i < placeHolderValues.length && i < data.length; i++) {
-        const placeholder = placeHolderValues[i].toString();
-        const value = data[i].toString();
-
+        const placeholder = placeHolderValues[i].toString()
+        const value = data[i].toString()
         await landingPage.fillUserFormDataMandatory(placeholder, value)
     }
+
+    const birthDate = getDateMinusFourYears(); // I am not sure about TS convention here. Should it be const birthDate: string = await... ?
+    
+    await landingPage.setDateForCalendar(birthDate)
+
+    const dateVerificator = await landingPage.formContainer.getByPlaceholder('Data urodzenia').inputValue()
+    expect(dateVerificator).toEqual(birthDate)
+     
 })
