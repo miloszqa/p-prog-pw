d7a31c7cc3f6bddfa5a20b0a03b8e314c426a171
Mil Ban <kutasaki1@wp.pl>
2024-09-07 14:43:48 +0200

    Create setDateForCalendar() method for calendar input field

--- page-object-model/landingPage.ts
+++ page-object-model/landingPage.ts
@@ -28,4 +28,15 @@ export class LandingPage {
     async fillUserFormDataMandatory(placeHolder: string, fieldsValue: string) {
         await this.formContainer.getByPlaceholder(placeHolder, { exact: true }).fill(fieldsValue)
     }
+
+    /**
+     * 
+     * @param inputDate this value is set to be string format YYYY-MM-DD auto generated from a function
+     */
+    async setDateForCalendar(inputDate: string) {
+        await this.formContainer.getByPlaceholder('Data urodzenia').click()
+        this.formContainer.getByPlaceholder('Data urodzenia').fill(inputDate)
+        await this.pageContainer.getByText('Załóż konto').click({ position: {x: 1, y: 1}});
+
+    }
 }
